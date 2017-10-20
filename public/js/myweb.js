65a9d206e1ad726833ff78b8693f506596ca1b07
Danny Dang <dnd003@ucsd.edu>
2017-10-19 23:30:57 -0700

    added new tab icon, back to top button, text size changes, i think thats it

--- public/js/myweb.js
+++ public/js/myweb.js
@@ -1,9 +1,27 @@
+window.onscroll = function() {scrollFunction()};
+
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("toTheTop").style.display = "block";
+    } else {
+        document.getElementById("toTheTop").style.display = "none";
+    }
+}
+
+
 $('a').click(function(){
     $('html, body').animate({
         scrollTop: $( $(this).attr('href') ).offset().top
-    }, 800);
+    }, 500);
     return false;
 });
 
 
 
+
+
+
+ 
+
+ 
